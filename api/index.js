8e2d56cfc712e8fcbc5f7c2d19db8991fb3d5b36
ashutosh sahu <ppaskumar@gmail.com>
2025-04-25 06:23:28 +0530

    index.js added

--- api/index.js
+++ api/index.js
@@ -1,20 +1,3 @@
-import fetch from "node-fetch";
-
-export default async function handler(req, res) {
-  try {
-    const swiggyURL =
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-    const response = await fetch(swiggyURL, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-        Accept: "application/json",
-      },
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    console.error("Error:", err);
-    res.status(500).json({ error: "Something went wrong" });
-  }
+export default function handler(req, res) {
+  res.json({ message: "Hello from API" });
 }
