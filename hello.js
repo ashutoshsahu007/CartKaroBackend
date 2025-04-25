fb0f2c78dd5e33f9243b5200600eb3f2efcb8418
ashutosh sahu <ppaskumar@gmail.com>
2025-04-25 06:12:08 +0530

    serverless file structure

--- hello.js
+++ hello.js
@@ -49,25 +49,6 @@ app.listen(PORT, () => {
   console.log(`Proxy running at http://localhost:${PORT}`);
 });
 
-// app.get("/menu", async (req, res) => {
-//   try {
-//     const swiggyURL =
-//       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
-//     const response = await fetch(swiggyURL, {
-//       headers: {
-//         "User-Agent":
-//           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-//         Accept: "application/json",
-//       },
-//     });
-//     const data = await response.json();
-//     res.json(data);
-//   } catch (err) {
-//     console.log("Error fetching data from Swiggy:", err);
-//     res.status(500).json({ error: "Something went wrong" });
-//   }
-// });
-
 app.get("/menu", async (req, res) => {
   const { restaurantId } = req.query;
   if (!restaurantId) {
